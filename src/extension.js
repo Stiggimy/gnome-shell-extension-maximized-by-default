9bed69004d99c922a047742ea4ba6da142627b24
Robert Wolff <mahlzahn@posteo.de>
2025-11-17 13:28:47 +0100

    fix: maximize windows only after they appear focused

--- src/extension.js
+++ src/extension.js
@@ -3,15 +3,16 @@ import Meta from 'gi://Meta';
 export default class MaximizedByDefaultExtension {
     enable() {
         global.display.connectObject('window-created', (display, window) => {
-            // Checks if the window is of "NORMAL" type (an actual app)
-            // Also checks if the window can actually be maximized
-            if (window.get_window_type() === Meta.WindowType.NORMAL && window.can_maximize()) {
-                window.maximize(Meta.MaximizeFlags.HORIZONTAL | Meta.MaximizeFlags.VERTICAL);
-            }
+            window?.connectObject('shown', window => {
+                if (window?.get_window_type() === Meta.WindowType.NORMAL && window?.can_maximize()) {
+                    window?.maximize(Meta.MaximizeFlags.BOTH);
+                    window?.disconnectObject(this);
+                }
+            }, this);
         }, this);
     }
 
     disable() {
-            global.display.disconnectObject(this);
+        global.display.disconnectObject(this);
     }
 }
